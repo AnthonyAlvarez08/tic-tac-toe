58698e4f90c824449045df95aa9c9a61b4d98f49
wataflai <anthonygenio2005@gmail.com>
2021-06-24 20:47:22 -0400

    fixed minor oversights, declared game end func

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -2,9 +2,9 @@
 #include <stdio.h>
 using namespace std;
 
-// the players
-const char X = 'X';
-const char O = 'O';
+/*
+TODO: win conditions and game ending
+*/
 
 // the game board
 char board[3][3] = {
@@ -23,24 +23,32 @@ void print_board();
 // function to make a play
 void make_play(char, int);
 
+// has game ended
+bool game_ended();
+
 int main() 
 {
     cout << "hello, and welcome to tic tac toe" << endl;
     cout << "This is how the spots are labeled:" << endl;
     cout << " 1 | 2 | 3 \n" << wall << " 4 | 5 | 6 \n" << wall << " 7 | 8 | 9 \n" << endl;
-    print_board();
 
     // main game loop
-    // for (char i : turns)
-    // {
-    //     char player = i;
-    //     cout << "here is the current board:\n";
-    //     print_board();
-    //     cout << "it is player " << player << "'s turn!" << endl << "choose a space from 1 to 9:  ";
-    //     int space;
-    //     cin >> space;
-    //     make_play(player, space);
-    // }
+    for (char i : turns)
+    {
+        char player = i;
+        cout << "here is the current board:\n";
+        print_board();
+        cout << "it is player " << player << "'s turn!" << endl << "choose a space from 1 to 9:  ";
+        int space;
+        cin >> space;
+        make_play(player, space);
+
+        if (game_ended())
+        {
+            cout << "the game ended!\n Here is the final board:\n";
+            print_board();
+        }
+    }
 
     return 0;
 }
@@ -51,26 +59,32 @@ void make_play(char player, int space)
     int col = space % 3;
     int row = space / 3; // int division on purpose;
 
-    // recursively make plays if invalid play
-    if (board[row][col] != ' ')
+    // do not let them make an invalid play
+    while (board[row][col] != ' ')
     {
         cout << "that is an invalid play, ";
         cout << "choose a space from 1 to 9:  ";
         int newspace;
         cin >> newspace;
-        make_play(player, newspace);
+        newspace -= 1;
+        col = newspace % 3;
+        row = newspace / 3;
     }
-    else
-    {
-        board[row][col] = player;
-    }
+    
+    board[row][col] = player;
+    
 
 }
 
 void print_board()
 {
-    printf(" %c | %c | %c \n-----------\n %c | %c | %c \n-----------\n %c | %c | %c \n", 
+    printf(" %c | %c | %c \n-----------\n %c | %c | %c \n-----------\n %c | %c | %c \n\n", 
         board[0][0], board[0][1], board[0][2], 
         board[1][0], board[1][1], board[1][2],
-        board[2][2], board[2][1], board[2][2]);
+        board[2][0], board[2][1], board[2][2]);
+}
+
+bool game_ended()
+{
+    return false;
 }
