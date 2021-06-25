5d5897d81f4f98e3327a3aed3ad7fddaa64f7f49
wataflai <anthonygenio2005@gmail.com>
2021-06-24 20:33:42 -0400

    made a good print board method

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 // the players
@@ -27,18 +28,19 @@ int main()
     cout << "hello, and welcome to tic tac toe" << endl;
     cout << "This is how the spots are labeled:" << endl;
     cout << " 1 | 2 | 3 \n" << wall << " 4 | 5 | 6 \n" << wall << " 7 | 8 | 9 \n" << endl;
+    print_board();
 
     // main game loop
-    for (char i : turns)
-    {
-        char player = i;
-        cout << "here is the current board:\n";
-        print_board();
-        cout << "it is player " << player << "'s turn!" << endl << "choose a space from 1 to 9:  ";
-        int space;
-        cin >> space;
-        make_play(player, space);
-    }
+    // for (char i : turns)
+    // {
+    //     char player = i;
+    //     cout << "here is the current board:\n";
+    //     print_board();
+    //     cout << "it is player " << player << "'s turn!" << endl << "choose a space from 1 to 9:  ";
+    //     int space;
+    //     cin >> space;
+    //     make_play(player, space);
+    // }
 
     return 0;
 }
@@ -67,5 +69,8 @@ void make_play(char player, int space)
 
 void print_board()
 {
-
+    printf(" %c | %c | %c \n-----------\n %c | %c | %c \n-----------\n %c | %c | %c \n", 
+        board[0][0], board[0][1], board[0][2], 
+        board[1][0], board[1][1], board[1][2],
+        board[2][2], board[2][1], board[2][2]);
 }
