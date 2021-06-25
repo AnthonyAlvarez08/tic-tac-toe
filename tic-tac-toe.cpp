a999d598777d6a87b937c5baf206a30add4bcfbf
wataflai <anthonygenio2005@gmail.com>
2021-06-24 20:13:42 -0400

    added gitignore & printed out rules

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+using namespace std;
+
+// the players
+const char X = 'X';
+const char O = 'O';
+
+// the game board
+char board[3][3];
+
+
+const string turns = "XOXOXOXOX"; // save the turns as a string so easy transition
+const string wall = "-----------\n";
+
 
 int main() 
 {
-    
+    cout << "hello, and welcome to tic tac toe" << endl;
+    cout << "This is how the spots are labeled:" << endl;
+    cout << " 1 | 2 | 3 \n" << wall << " 4 | 5 | 6 \n" << wall << " 7 | 8 | 9 \n" << endl;
+
+    // main game loop
+    for (char i : turns)
+    {
+        char player = i;
+        cout << "it is player " << player << "'s turn!" << endl << "choose a space from 1 to 9" << endl;
+    }
+
+    return 0;
 }
